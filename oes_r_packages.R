fcdd51bbfbc1cfb8d19e2ef4fc8404ca9cb85334
JacobWBowers <5600852864@GSA.GOV>
2022-10-28 09:19:30 -0500

    Add languageserver, add an R project, add a .gitignore.

--- oes_r_packages.R
+++ oes_r_packages.R
@@ -2,7 +2,11 @@
 ## This file helps install our R environment
 
 
-install.packages(c("tidyverse", "optmatch", "blocktools", "designmatch", "RItools", "coin", "DeclareDesign", "lme4", "brms", "rstanarm", "here", "blkvar", "lfe", "fixest", "data.table", "dtplyr", "estimatr", "quarto", "rmarkdown", "bookdown", "tinytex", "renv", "styler","quickmatch","nbpMatching"), dependencies = TRUE)
+install.packages(c("tidyverse", "optmatch", "blocktools", "designmatch", "RItools",
+                   "coin", "DeclareDesign", "lme4", "brms", "rstanarm", "here", 
+                   "blkvar", "lfe", "fixest", "data.table", "dtplyr", "estimatr", 
+                   "quarto", "rmarkdown", "bookdown", "tinytex", "renv", "styler",
+                   "quickmatch", "nbpMatching","languageserver"), dependencies = TRUE)
 
 ## You might need to install some other pieces one at a time:
 
@@ -14,8 +18,8 @@ tinytex:::install_yihui_pkgs()
 
 ## Test tinytex
 writeLines(c(
-  '\\documentclass{article}',
-  '\\begin{document}', 'Hello world!', '\\end{document}'
-), 'test.tex')
+  "\\documentclass{article}",
+  "\\begin{document}", "Hello world!", "\\end{document}"
+), "test.tex")
 
-tinytex::pdflatex('test.tex')
+tinytex::pdflatex("test.tex")
