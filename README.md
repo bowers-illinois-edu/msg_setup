8696c0f87746d95f19c8a522deb90b73b7f563ed
Jake Bowers <jake@jakebowers.org>
2022-10-28 09:05:52 -0500

    Add initial files.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Files to help us create a data analysis environment
+
+We hope that eventually one can execute these scripts to install and verify the installation of our main tools.
+
+# R based workflow
+
+For now we are focusing on R. If you find yourself needing a package that is not installed here, please add it.
---
+++ oes_r_packages.R
@@ -0,0 +1,21 @@
+
+## This file helps install our R environment
+
+
+install.packages(c("tidyverse", "optmatch", "blocktools", "designmatch", "RItools", "coin", "DeclareDesign", "lme4", "brms", "rstanarm", "here", "blkvar", "lfe", "fixest", "data.table", "dtplyr", "estimatr", "quarto", "rmarkdown", "bookdown", "tinytex", "renv", "styler","quickmatch","nbpMatching"), dependencies = TRUE)
+
+## You might need to install some other pieces one at a time:
+
+## blockTools
+
+## Get tinytex working for our reports
+tinytex::install_tinytex()
+tinytex:::install_yihui_pkgs()
+
+## Test tinytex
+writeLines(c(
+  '\\documentclass{article}',
+  '\\begin{document}', 'Hello world!', '\\end{document}'
+), 'test.tex')
+
+tinytex::pdflatex('test.tex')
